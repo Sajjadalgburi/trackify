b70ec9f7556f5dcbe82ec874b997830d0ca7943c
Sajjadalgburi <dfgsajjad@gmail.com>
2024-08-09 19:45:29 -0400

    feat: Add error handling for database seeding

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,29 @@
+"use client";
+
 const page = () => {
-  return <div>page</div>;
+  const handleClick = async () => {
+    try {
+      const res = await fetch("/api/seed");
+
+      if (!res.ok) {
+        throw new Error(`Error in seeding: ${res.statusText}`);
+      }
+
+      console.log("It worked successfully");
+    } catch (error) {
+      console.error("Error in seeding", error);
+      // Optionally show a user-friendly message
+      alert("There was an error seeding the database. Please try again.");
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={handleClick} className="btn">
+        Click me
+      </button>
+    </div>
+  );
 };
 
 export default page;
