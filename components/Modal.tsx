1632582e022b16d495bbf3bb4bb6d68ef3872384
Sajjadalgburi <dfgsajjad@gmail.com>
2024-08-12 18:24:42 -0400

    feat: Add Modal component for displaying login and register forms

---
+++ components/Modal.tsx
@@ -0,0 +1,25 @@
+import LoginOrSignUpForm from "./LoginOrSignUpForm";
+
+interface ModalProps {
+  type: "login" | "register";
+}
+
+const Modal = ({ type }: ModalProps) => {
+  return (
+    <dialog id="my_modal" className="modal">
+      <div className="modal-box">
+        <form method="dialog">
+          {/* if there is a button in form, it will close the modal */}
+          <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+            ✕
+          </button>
+        </form>
+
+        {/* Conditionally rendering the form based on the 'register' prop */}
+        <LoginOrSignUpForm type={type} />
+      </div>
+    </dialog>
+  );
+};
+
+export default Modal;
