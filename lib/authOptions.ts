b0eabec9d70b9a9c204d00a93663b071aa9cbf48
Sajjadalgburi <dfgsajjad@gmail.com>
2024-08-12 19:07:06 -0400

    refactor: Update authentication callbacks and user session handling

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -1,11 +1,15 @@
-import { NextAuthOptions } from "next-auth";
-
 // these are providers that we will use for authentication
 import GithubProvider from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 
+// import mongodb
+import { connectToMongoDb } from "@/lib/database";
+
+// user modal
+import { User } from "@/models/user";
+
 // intiliazing the options for the authentication and passing some type references
-export const authOptions: NextAuthOptions = {
+export const authOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
@@ -17,4 +21,36 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
+  callbacks: {
+    async session({ session }) {
+      // store the user id from MongoDB to session
+      const sessionUser = await User.findOne({ email: session.user.email });
+      session.user.id = sessionUser._id.toString();
+
+      return session;
+    },
+    async signIn({ profile }) {
+      try {
+        await connectToMongoDb();
+        // 1. Check if user already exists in the database
+        // 2. If not, create a new user in the database
+
+        const existingUser = await User.findOne({ email: profile.email });
+
+        if (!existingUser) {
+          await User.create({
+            email: profile.email,
+            username: profile.username.replace(" ", "").toLowerCase(), // remove spaces and convert username to lowercase
+            password: profile.password,
+            image: profile.image,
+          });
+        }
+
+        return true;
+      } catch (error) {
+        console.error(error);
+        return false;
+      }
+    },
+  },
 };
