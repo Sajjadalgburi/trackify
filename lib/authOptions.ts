acb05bc83d1e32d34066f517a68dd544422f1152
Sajjadalgburi <dfgsajjad@gmail.com>
2024-08-13 16:57:03 -0400

    refactor: Update authOptions.ts to include CredentialsProvider for username and password authentication

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -1,17 +1,20 @@
 // these are providers that we will use for authentication
 import GithubProvider from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 
-// import mongodb
+// mongodb and the user modal
 import { connectToMongoDb } from "@/lib/database";
+import { UserModel } from "@/models/user";
 
-// user modal
-import { User } from "@/models/user";
+// bcrypt for hashing the password before saving it to the database
+import bcrypt from "bcrypt";
 
-import { Session } from "next-auth";
+// importing the NextAuthOptions type from next-auth
+import { NextAuthOptions } from "next-auth";
 
 // intiliazing the options for the authentication and passing some type references
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
@@ -22,55 +25,77 @@ export const authOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
-  ],
-  callbacks: {
-    // TODO: Implement type so that session.user.id is not any
-    async session({ session }: { session: Session }) {
-      // store the user id from MongoDB to session
-      const sessionUser = await User.findOne({ email: session.user.email });
-      session.user.id = sessionUser._id.toString();
 
-      return session;
-    },
-    async signIn({
-      profile,
-    }: {
-      profile: {
-        name: string;
-        email: string;
-        username: string;
-        password: string;
-        picture?: string | null;
-      };
-    }) {
-      // log the profile object to the console
-      console.log("============================================");
-      console.log("============================================");
-      console.log(profile);
-      console.log("============================================");
-      console.log("============================================");
+    CredentialsProvider({
+      // The name to display on the sign-in form (e.g. "Sign in with...")
+      name: "Enter your credentials",
 
-      try {
-        await connectToMongoDb();
-        // 1. Check if user already exists in the database
-        const existingUser = await User.findOne({ email: profile.email });
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        email: {
+          label: "Email",
+          type: "text",
+          placeholder: "jsmith@example.com",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "*******",
+        },
+      },
 
-        // 2. If not, create a new user in the database
-        if (!existingUser) {
-          await User.create({
-            email: profile.email,
-            username: profile.name.replace(" ", "").toLowerCase(), // remove spaces and convert username to lowercase
-            password: profile.password,
-            image: profile.picture,
-          });
+      async authorize(
+        credentials:
+          | Record<"email" | "username" | "password", string>
+          | undefined,
+        req: any
+      ): Promise<any> {
+        if (!credentials) {
+          throw new Error("No credentials provided");
         }
 
-        return true;
-      } catch (error) {
-        // return false if there is an error in the sign in process
-        console.error(error);
-        return false;
-      }
-    },
-  },
+        try {
+          await connectToMongoDb();
+
+          // Look up the user from the credentials supplied
+          let user = await UserModel.findOne({ email: credentials.email });
+
+          if (!user) {
+            // If user does not exist, create a new user
+            const hashedPassword = await bcrypt.hash(credentials.password, 10);
+
+            if (!hashedPassword) {
+              throw new Error("Password hashing failed");
+            }
+
+            user = await UserModel.create({
+              email: credentials.email,
+              username: credentials.username
+                .replace(" ", "")
+                .toLowerCase()
+                .trim(),
+              password: hashedPassword,
+            });
+
+            return { id: user._id, email: user.email, username: user.username };
+          }
+
+          // Check if the password is correct
+          const isPasswordCorrect = await bcrypt.compare(
+            credentials.password.trim(),
+            user.password
+          );
+
+          if (!isPasswordCorrect) {
+            throw new Error("Invalid credentials");
+          }
+
+          return { id: user._id, email: user.email, username: user.username };
+        } catch (error) {
+          console.error("Authorization error:", error);
+          return null;
+        }
+      },
+    }),
+  ],
 };
