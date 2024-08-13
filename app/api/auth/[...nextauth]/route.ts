60af1d19de8df9e30e235f06cae74423f4441d02
Sajjadalgburi <dfgsajjad@gmail.com>
2024-08-13 17:00:17 -0400

    refactor: Modularize callback functions for better readability and maintainability

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,19 @@
 import NextAuth from "next-auth/next";
 import { authOptions } from "@/lib/authOptions";
 
-export const handler = NextAuth(authOptions);
+// callback functions modularized for better readability
+import { signIn } from "@/callbacks/signInCallback";
+import { session } from "@/callbacks/sessionCallback";
+
+export const handler = NextAuth({
+  // passing the spread operator to pass all the options from the authOptions
+  ...authOptions,
+
+  callbacks: {
+    session,
+    signIn,
+  },
+});
 
 // exporting the handler for the api route as both GET and POST
 export { handler as GET, handler as POST };
