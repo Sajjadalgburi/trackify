2291bcdff92c3c69dbd2ae502d6a342c7d15c81c
Sajjadalgburi <dfgsajjad@gmail.com>
2024-08-13 12:01:50 -0400

    refactor: Update Navbar component to use the session to check wether the user is signed in to update the navbar state

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,20 +2,12 @@
 
 import { navbarItems } from "@/data";
 import Link from "next/link";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import MobileNavigation from "./MobileNavigation";
 import LoginOrSignUpBtn from "./LoginOrSignUpBtn";
 
 // bunch of imports from next-auth to handle the session and authentication
-import {
-  signIn,
-  signOut,
-  useSession,
-  getProviders,
-  ClientSafeProvider,
-  LiteralUnion,
-} from "next-auth/react";
-import { BuiltInProviderType } from "next-auth/providers/index";
+import { useSession, signOut } from "next-auth/react";
 
 const Navbar = () => {
   // usestate to toggle the mobile navigation
@@ -28,33 +20,15 @@ const Navbar = () => {
     setToggle((prev) => !prev);
   };
 
-  // fetching the providers from the getProviders function
-  const [providers, setProviders] =
-    // setting the type of the state to the Record of the providers.
-    // if this is not set, the typescript will throw an error
-    useState<Record<
-      LiteralUnion<BuiltInProviderType, string>,
-      ClientSafeProvider
-    > | null>(null);
-
-  useEffect(() => {
-    (async () => {
-      const res = await getProviders();
-      setProviders(res);
-    })();
-  }, []);
-
-  console.log(providers);
-
   return (
     <header className="max-w-[90vw] w-full mx-auto p-1 md:px-10 md:py-3  ">
       <nav className="flex justify-between items-center">
-        {/*  */}
+        {/* Desktop Navigation */}
         <div className="flex justify-center items-center gap-3">
           <Link className="md:mr-6" href="/">
             <h1 className="text-4xl font-bold">Trackify.me</h1>{" "}
           </Link>
-          {/* Desktop Navigation */}{" "}
+
           <ul className="md:flex hidden justify-center items-center gap-4">
             {navbarItems.map((item, index) => (
               <li className="navbar_items" key={index}>
@@ -62,21 +36,31 @@ const Navbar = () => {
               </li>
             ))}
           </ul>
-          {/* This below is for mobile navigation */}
         </div>
-
         <div className="md:flex hidden justify-center items-center gap-4">
-          {/* Replace with actual custom buttons */}
-
-          <LoginOrSignUpBtn />
+          {session?.user ? (
+            <>
+              {/* logout button */}
+              <button onClick={() => signOut()} className="btn btn-accent">
+                Logout
+              </button>
+              {/* show dashboard button if user is logged in */}
+              <button className="btn btn-neutral">
+                <Link href="/dashboard">Dashboard</Link>
+              </button>{" "}
+            </>
+          ) : (
+            // show login or signup button if user is not logged in
+            <LoginOrSignUpBtn />
+          )}
         </div>
-
-        {/* passing the state into the mobile navigation */}
+        {/* Desktop Navigation  */}
+        {/*  */}
+        {/* This below is for mobile navigation */}
         <MobileNavigation
           handleToggleChange={handleToggleChange}
           toggle={toggle}
         />
-
         {/*  */}
       </nav>
     </header>
