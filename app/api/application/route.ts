7d0008d74f4118d34d10b297b32b7d984030d47a
Sajjadalgburi <dfgsajjad@gmail.com>
2024-08-14 18:51:19 -0400

    refactor: Add API route for fetching applications

---
+++ app/api/application/route.ts
@@ -0,0 +1,30 @@
+import { connectToMongoDb } from "@/lib/database";
+import { ApplicationModel } from "@/models/application";
+
+export const GET = async () => {
+  try {
+    await connectToMongoDb();
+    const applications = await ApplicationModel.find({});
+
+    if (!applications) {
+      return new Response(
+        JSON.stringify({ message: "No applications found" }),
+        {
+          status: 404,
+        }
+      );
+    }
+
+    return new Response(JSON.stringify(applications), {
+      status: 200,
+    });
+  } catch (error) {
+    console.error("Error in fetching applications", error);
+    return new Response(
+      JSON.stringify({ message: "Error in fetching applications" }),
+      {
+        status: 500,
+      }
+    );
+  }
+};
