012ebf3b23409640d5d6da1748bb6856a67353a8
Sajjadalgburi <dfgsajjad@gmail.com>
2024-08-17 16:16:46 -0400

    refactor: Update Card component to include link to edit application details

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image"; // Make sure to import the Image component
+import Link from "next/link";
 
 import { ApplicationInterface } from "@/interfaces";
 
@@ -7,32 +8,38 @@ interface CardProps {
   applicationArray: ApplicationInterface[];
 }
 
+import { dateDiffInDays } from "@/lib/helper";
+
 const Card: React.FC<CardProps> = ({ applicationArray }) => {
   return (
     <div className="flex flex-col gap-y-10 mt-[3rem]">
       {applicationArray.map((app) => (
-        <div className="rounded bg-base-100 p-4 w-full border border-base-300">
-          <div key={app.id} className="flex items-start">
-            <div className="bg-white h-[100px] w-1"></div>
-            <div className="p-2">
-              <h2 className="text-xl text-base-content font-medium">
-                {app.company}
-              </h2>
-              <div className="flex items-center justify-start mt-2">
-                <Image
-                  alt="logo"
-                  src={app.logo || "/job_placeholder.svg"}
-                  className="mr-4"
-                  width={30}
-                  height={30}
-                />
-                <p className="text-sm text-base-content opacity-70">
-                  {app.position}
+        <Link
+          key={app._id as string}
+          href={`/dashboard/edit-application?id=${app._id as string}`}
+        >
+          <div className="relative rounded-xl hover:border-secondary hover:border-solid transition-all cursor-pointer bg-slate-100 p-2 w-full border-2 border-base-300">
+            <div className="flex text-black items-start">
+              <div className="bg-secondary rounded-md h-[80px] w-1"></div>
+              <div className="p-2">
+                <h2 className="text-lg font-medium">{app.company}</h2>
+                <div className="flex items-center justify-start mt-2">
+                  <Image
+                    alt="logo"
+                    src={app.logo || "/job_placeholder.svg"}
+                    className="mr-4"
+                    width={30}
+                    height={30}
+                  />
+                  <p className="text-sm opacity-70">{app.position}</p>
+                </div>
+                <p className="absolute bottom-0 right-0 text-sm p-2 text-gray-500">
+                  {dateDiffInDays(app.date as string, "any status")}
                 </p>
               </div>
             </div>
           </div>
-        </div>
+        </Link>
       ))}
     </div>
   );
