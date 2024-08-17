ba981533e0549697b1af949f8938d623fa7c1a90
Sajjadalgburi <dfgsajjad@gmail.com>
2024-08-17 16:12:10 -0400

    refactor: Update PATCH route to handle application updates

--- app/api/application/[id]/route.ts
+++ app/api/application/[id]/route.ts
@@ -45,3 +45,55 @@ export const GET = async (
     return new NextResponse("Couldn't get the application", { status: 500 });
   }
 };
+
+export const PATCH = async (
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  try {
+    await connectToMongoDb();
+
+    const {
+      company,
+      position,
+      status,
+      date,
+      note,
+      url,
+      logo,
+      location,
+      userId,
+    } = await req.json();
+
+    const user = await User.findOneAndUpdate(
+      { _id: userId, "applications._id": params.id },
+      {
+        $set: {
+          "applications.$.company": company,
+          "applications.$.position": position,
+          "applications.$.status": status,
+          "applications.$.date": date,
+          "applications.$.note": note,
+          "applications.$.url": url,
+          "applications.$.logo": logo,
+          "applications.$.location": location,
+        },
+      },
+      { new: true }
+    );
+
+    if (!user) {
+      return new NextResponse(JSON.stringify({ message: "User not found" }), {
+        status: 404,
+      });
+    }
+
+    return new NextResponse(
+      JSON.stringify({ message: "Application updated successfully" }),
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(error);
+    return new NextResponse("Couldn't update the application", { status: 500 });
+  }
+};
