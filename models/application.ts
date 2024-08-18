b095906e7c73760511a926a32cfc9903b58c6982
Sajjadalgburi <dfgsajjad@gmail.com>
2024-08-18 16:40:31 -0400

    refactor: Update application schema to include timestamps

--- models/application.ts
+++ models/application.ts
@@ -2,38 +2,41 @@ import { Schema } from "mongoose";
 import { ApplicationStatus, ApplicationInterface } from "@/interfaces";
 
 // Create the application schema
-export const applicationSchema = new Schema<ApplicationInterface>({
-  position: {
-    type: String,
-    required: [true, "Position is required"],
-  },
+export const applicationSchema = new Schema<ApplicationInterface>(
+  {
+    position: {
+      type: String,
+      required: [true, "Position is required"],
+    },
 
-  // Use the enum values for the status field
-  status: {
-    type: String,
-    enum: Object.values(ApplicationStatus),
-    default: ApplicationStatus.APPLIED,
-    required: [true, "Status is required"],
+    // Use the enum values for the status field
+    status: {
+      type: String,
+      enum: Object.values(ApplicationStatus),
+      default: ApplicationStatus.APPLIED,
+      required: [true, "Status is required"],
+    },
+    date: {
+      type: String,
+      default: Date.now,
+      required: [false, "Date is required"],
+    },
+    company: {
+      type: String,
+      required: [true, "Company is required"],
+    },
+    note: {
+      type: String,
+    },
+    url: {
+      type: String,
+    },
+    logo: {
+      type: String,
+    },
+    location: {
+      type: String,
+    },
   },
-  date: {
-    type: String,
-    default: Date.now,
-    required: [false, "Date is required"],
-  },
-  company: {
-    type: String,
-    required: [true, "Company is required"],
-  },
-  note: {
-    type: String,
-  },
-  url: {
-    type: String,
-  },
-  logo: {
-    type: String,
-  },
-  location: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
