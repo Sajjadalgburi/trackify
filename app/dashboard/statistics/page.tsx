c31937c9c4380b7c48a68d671e39dd106421305e
Sajjadalgburi <dfgsajjad@gmail.com>
2024-08-18 16:41:07 -0400

    refactor: Add lazy loading for images in Statistics page

--- app/dashboard/statistics/page.tsx
+++ app/dashboard/statistics/page.tsx
@@ -4,6 +4,9 @@ import React, { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import DashboardLayout from "../DashboardLayout";
 import { ApplicationInterface } from "@/interfaces";
+import { GirdItem } from "@/components/GridItem";
+import LineChart from "@/components/LineChart";
+import { convertIntoDate } from "@/lib/helper";
 
 const Page = () => {
   const [application, setApplication] = useState<ApplicationInterface[]>([]);
@@ -36,12 +39,39 @@ const Page = () => {
     fetchApplications();
   }, [userId]);
 
-  console.log(application);
+  // map through the application and get the date in string format such as "2021-09-01"
+  const getDatesFromApplication = application.map(
+    (app) => app?.createdAt as string
+  );
+
+  // use the helper function to iterate through each date and increment the month apperance => 'aug': 2, 'sep':1, 'oct': 14
+  const applicationCountByMonth = convertIntoDate(getDatesFromApplication);
+
+  // Convert the Map into an array of objects for the LineChart
+  const chartData = Array.from(applicationCountByMonth, ([month, count]) => ({
+    month,
+    count,
+  }));
 
   return (
     <DashboardLayout>
-      <div>
-        <h1>Welcome to Statistics</h1>
+      <div className="max-w-[85%] mx-auto pt-20">
+        <div className="text-left ml-[8rem]">
+          <h1 className="text-3xl md:text-6xl font-bold">
+            Welcome to Statistics
+          </h1>
+          {/* p tag to explain the purpose */}
+          <p className="text-md mt-4">
+            Here you can see the statistics of your applications
+          </p>
+        </div>
+
+        {/* grid items which will display the charts */}
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-[7rem]">
+          <GirdItem title="Total Applications">
+            <LineChart appData={chartData} />
+          </GirdItem>
+        </div>
       </div>
     </DashboardLayout>
   );
