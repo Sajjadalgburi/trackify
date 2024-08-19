17611ba6333f9aed88fbcd7c18e5a9f4b7296b4a
Sajjadalgburi <dfgsajjad@gmail.com>
2024-08-19 13:50:40 -0400

    refactor: Update GridItem component to include description prop

--- components/GridItem.tsx
+++ components/GridItem.tsx
@@ -3,12 +3,18 @@ import React from "react";
 interface GirdItemProps {
   title: string;
   children: React.ReactNode;
+  desc: string;
 }
 
-export const GirdItem: React.FC<GirdItemProps> = ({ title, children }) => {
+export const GirdItem: React.FC<GirdItemProps> = ({
+  title,
+  children,
+  desc,
+}) => {
   return (
-    <div className="flex flex-col justify-center">
-      <p className="text-3xl lowercase">{title}</p>
+    <div className="flex flex-col justify-center gap-4">
+      <p className="text-2xl font-medium capitalize">{title}</p>
+      <p className="text-sm font-thin">{desc}</p>
       <div className="bg-base-200 p-8 rounded-3xl shadow-md h-[530px]">
         {children}
       </div>
