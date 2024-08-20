7a06a200256f1bb297153943cf36e100a72cf2cf
Sajjadalgburi <dfgsajjad@gmail.com>
2024-08-20 11:31:11 -0400

    refactor: Add delete functionality for applications in Dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -46,6 +46,37 @@ const Page = () => {
     fetchApplications();
   }, [userId]);
 
+  const handleDelete = async (id: string) => {
+    try {
+      if (!userId) return;
+
+      const hasConfirmed = confirm(
+        "Are you sure you want to delete this application?"
+      );
+
+      if (!hasConfirmed) return;
+
+      // delete the application from the database
+      const response = await fetch(`/api/application/${id}`, {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${userId}`,
+        },
+      });
+      // if the response is not ok, throw an error
+      if (!response.ok) {
+        throw new Error("An error occurred while deleting the application");
+      }
+      // filter the application state and remove the deleted application
+      setApplication((prevState) => prevState.filter((app) => app._id !== id));
+    } catch (error) {
+      console.error(
+        "An error occurred during deleting the application:",
+        error
+      );
+    }
+  };
+
   // filter the applications based on the status and pass each status to the appropriate card
   return (
     <DashboardLayout>
@@ -53,22 +84,27 @@ const Page = () => {
         <CardElement
           application={filterByStatus(application, "offer")}
           type="offer"
+          handleDelete={handleDelete}
         />
         <CardElement
           application={filterByStatus(application, "applied")}
           type="applied"
+          handleDelete={handleDelete}
         />
         <CardElement
           application={filterByStatus(application, "interview")}
           type="interview"
+          handleDelete={handleDelete}
         />
         <CardElement
           application={filterByStatus(application, "rejected")}
           type="rejected"
+          handleDelete={handleDelete}
         />
         <CardElement
           application={filterByStatus(application, "pending")}
           type="pending"
+          handleDelete={handleDelete}
         />
       </div>
     </DashboardLayout>
